42e21496e4aa1ac2bf2f4ae6c6662bfdbfa5b2b8
Yash Patel <yash1451999@gmail.com>
2024-04-02 20:39:33 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Footer from "../Component/Footer";
+import Navbar from "../Component/Navbar";
+import Recipe from "../Component/Recipe";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [recipes, setRecipes] = useState({});
+  useEffect(() => {
+    fetch("https://api.sampleapis.com/recipes/recipes")
+      .then((res) => {
+        console.log("unmounted");
+        return res.json();
+      })
+      .then((data) => {
+        setRecipes(data);
+      });
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      {recipes.length &&
+        recipes.map((data) => <Recipe recipe={data} key={data.id} />)}
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
